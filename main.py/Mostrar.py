f4911de107e031ac256ce728ffedc58355d88181
hurluis <luismiguelgiraldohurtado@gmail.com>
2024-05-16 22:32:42 -0500

    new-branch

--- main.py/Mostrar.py
+++ main.py/Mostrar.py
@@ -7,7 +7,12 @@ class Mostrar:
         
     def mostrarPaciente(self):
         current = self.hospital.removeMin()
-        print(f"\nEl paciente que continua es: {current.nombre}.\n")
+        print(f"\n- El paciente que continua es: {current.nombre}.\n")
+        print(f"- Su identificador único es: {current.numeroPaciente}.\n")
+        print(f"- De genero: {current.genero}.\n")
+        print(f"- De {current.Edad} año(s).\n")
+        print(f"- Esta en el triaje {current.Triaje}.\n")
+        
         self.hospital.insert(current)
        
             
